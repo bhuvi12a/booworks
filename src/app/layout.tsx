68682dbe75d6efc0c6e51562836c34fe303a789c
bhuvi12a <bhuvi12a@gmail.com>
2026-01-19 20:27:35 +0530

    Add Google Analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -86,6 +86,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-H6QW5HDNME"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-H6QW5HDNME');
+            `,
+          }}
+        />
         {/* JSON-LD Structured Data */}
         <script
           type="application/ld+json"
